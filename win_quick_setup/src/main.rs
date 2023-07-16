23e530843c6c9eb63a47d548e699c4c05573d642
Mihai Andrei Gherghinescu <mihai.gherghinescu00@e-uvt.ro>
2023-07-16 10:41:36 +0300

    Added cmd line arguments passing and json parsing

--- win_quick_setup/src/main.rs
+++ win_quick_setup/src/main.rs
@@ -1,13 +1,40 @@
 mod rendering;
 
+use serde_json::Value;
 use std::error::Error;
-use serde_json::json;
+use std::fs::File;
+use std::io::Read;
+use std::env;
 
-use crate::rendering::run_command;
+use crate::rendering::{render, InstallActionType};
 
 fn main() -> Result<(), Box<dyn Error>> {
+
+    let args: Vec<String> = env::args().collect();
     
-    run_command(json!({ "exec": "winget install BurntSushi.ripgrep.MSVC" }))?;
+    if args.len() > 2 {
+        
+        let action: InstallActionType;
+        match &args[1].as_str() {
+            &"--install" => action = InstallActionType::INSTALL,
+            &"--uninstall" => action = InstallActionType::UNINSTALL,
+            &"--update" => action = InstallActionType::UPDATE,
+            _ => panic!("Invalid option passed {}", &args[1])
+        }
+        
+        let mut file = File::open(&args[2]).expect(&format!("Failed to open file {}", &args[2]));
+        let mut contents = String::new();
+        file.read_to_string(&mut contents)
+            .expect("Failed to read file");
     
-    Ok(())
+        let json_data: Value = serde_json::from_str(&contents).expect("Failed to parse JSON");
+    
+        render(&json_data, &action)?;
+    
+        Ok(())
+    }
+    else {
+        panic!("No arguments passed");
+    }
+    
 }
--- win_quick_setup/src/rendering.rs
+++ win_quick_setup/src/rendering.rs
@@ -1,39 +1,115 @@
 use serde_derive::{Deserialize, Serialize};
+use serde_json::{from_value, Value};
 use std::error::Error;
-use serde_json::{Value, from_value};
 use std::process::Command;
 
+pub enum InstallActionType {
+    INSTALL,
+    UNINSTALL,
+    UPDATE,
+}
 #[derive(Deserialize, Serialize)]
 struct JsonCommand {
-    exec: String,
+    install_run: String,
+    uninstall_run: String,
+    update_run: String,
 }
 
 impl JsonCommand {
-    pub fn execute(&self) -> Result<bool, Box<dyn Error>> {
-    
-        println!("Executing command: {}", self.exec);
-        
-        let succes: bool = if cfg!(target_os = "windows") {
+    pub fn execute(&self, action: &InstallActionType) -> Result<bool, Box<dyn Error>> {
+        let exec: &String;
+        match action {
+            InstallActionType::INSTALL => {
+                exec = &self.install_run;
+            }
+            InstallActionType::UNINSTALL => {
+                exec = &self.uninstall_run;
+            }
+            InstallActionType::UPDATE => {
+                exec = &self.update_run;
+            }
+        }
+
+        println!("Executing command: {}", exec);
+
+        let exitcode: Option<i32> = if cfg!(target_os = "windows") {
             Command::new("cmd")
-                    .args(&["/C", self.exec.as_str()])
-                    .status()
-                    .map(|status| status.success())
-                    .unwrap_or(false)
+                .args(&["/C", &exec.as_str()])
+                .status()
+                .map(|exitcode| exitcode.code())
+                .unwrap_or(Some(-1))
         } else {
             Command::new("sh")
-                    .arg("-c")
-                    .arg(&self.exec.as_str())
-                    .status()
-                    .map(|status| status.success())
-                    .unwrap_or(false)
+                .arg("-c")
+                .arg(&exec.as_str())
+                .status()
+                .map(|exitcode| exitcode.code())
+                .unwrap_or(Some(-1))
         };
-        
-        return Ok(succes);
+
+        if exec.starts_with("winget") {
+            // DUE TO WRONG EXIT CODE WHEN NO NEW PACKAGE FOUND...
+            // SHOULD AT LEAST BE SOMETHING LIKE
+            return Ok(true);
+        }
+
+        return Ok(exitcode.is_some_and(|x| x == 0));
     }
 }
 
-pub fn run_command(json_data: Value) -> Result<bool, Box<dyn Error>> {
-    let cmd: JsonCommand = from_value(json_data)?;
-    
-    return cmd.execute();
+type ActionFn = fn(&Value, &InstallActionType) -> Result<bool, Box<dyn Error>>;
+
+fn run_command(json_data: &Value, action: &InstallActionType) -> Result<bool, Box<dyn Error>> {
+    let cmd: JsonCommand = from_value(json_data.clone())?;
+
+    return cmd.execute(action);
+}
+
+const ACTION_MAP: &[(&str, ActionFn); 1] = &[("exec", run_command)];
+
+pub fn render(json_data: &Value, action: &InstallActionType) -> Result<bool, Box<dyn Error>> {
+    if let Value::Array(obj) = json_data {
+        for value in obj.iter() {
+            if let Value::Object(object) = value {
+                if object.len() != 1 {
+                    let json_string =
+                        serde_json::to_string(&object).expect("Failed to convert JSON to string");
+                    panic!(
+                        "Failed to found matching instruction for json: {}",
+                        json_string
+                    );
+                }
+
+                if let Some(first_key) = object.keys().next() {
+                    if let Some(&function) = ACTION_MAP
+                        .iter()
+                        .find(|&(key, _)| key == first_key)
+                        .map(|(_, function)| function)
+                    {
+                        let ok = (function)(&object[first_key], action)?;
+                        if !ok {
+                            println!("One of the instructions failed, halting execution");
+                            return Ok(false);
+                        }
+                    } else {
+                        let json_string = serde_json::to_string(&object)
+                            .expect("Failed to convert JSON to string");
+                        panic!(
+                            "Failed to found matching instruction for json: {}",
+                            json_string
+                        );
+                    }
+                } else {
+                    let json_string =
+                        serde_json::to_string(&object).expect("Failed to convert JSON to string");
+                    panic!(
+                        "Failed to found matching instruction for json: {}",
+                        json_string
+                    );
+                }
+            }
+        }
+    }
+
+    return Ok(true);
 }
